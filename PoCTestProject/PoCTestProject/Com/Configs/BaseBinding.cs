278a05b2a6077c5f2ac9d57089f2b026273fd787
Diana Otel <diana.andra.otel@gmail.com>
2017-10-19 15:57:29 +0300

    Added prints for threads

--- PoCTestProject/PoCTestProject/Com/Configs/BaseBinding.cs
+++ PoCTestProject/PoCTestProject/Com/Configs/BaseBinding.cs
@@ -3,6 +3,7 @@ using BoDi;
 using PoCTestProject.Com.Configs;
 using System;
 using System.Diagnostics;
+using System.Threading;
 using TechTalk.SpecFlow;
 
 namespace PoCTestProject.Com.Selenium
@@ -14,9 +15,12 @@ namespace PoCTestProject.Com.Selenium
         private readonly IObjectContainer objectContainerPrivate;
         private CWebDriver webdriver;
         private static ExtentReports report;
+        static ProcessThreadCollection initialThreadCount;
 
         public BaseBinding(IObjectContainer objectContainer)
         {
+            initialThreadCount = Process.GetCurrentProcess().Threads;
+            Console.WriteLine("Initial process number of threads: {0}", initialThreadCount.Count);
             objectContainerPrivate = objectContainer;
         }
 
@@ -41,6 +45,8 @@ namespace PoCTestProject.Com.Selenium
         [AfterScenario]
         public void TearDown()
         {
+            
+
             if (ScenarioContext.Current.TestError != null)
             {
                 //write report to file if errors are found
@@ -52,20 +58,29 @@ namespace PoCTestProject.Com.Selenium
             //close driver
             webdriver.GetDriver().Quit();
 
-            ProcessThreadCollection currentThreads = Process.GetCurrentProcess().Threads;
-            //System.Diagnostics.Debug.WriteLine("----*****----Here: " + currentThreads.Count);
-            Trace.WriteLine("----*****----Here: " + currentThreads.Count);
+            Console.WriteLine("Main program: {0}", System.Reflection.Assembly.GetExecutingAssembly().GetName().FullName);
 
-           // if (currentThreads.Count == 1)
+            Console.WriteLine("Current thread id: {0}", Thread.CurrentThread.ManagedThreadId);
+
+            ProcessThreadCollection currentThreads = Process.GetCurrentProcess().Threads;
+            Console.WriteLine("Current process number of threads: {0}", currentThreads.Count);
+
+            Console.WriteLine("Current process id: {0}", Process.GetCurrentProcess().Id);
+
+            //foreach (Process winProc in Process.GetProcesses())
+            //{
+            //    Console.WriteLine("Process {0}: {1}. Thread count: {2}", winProc.Id, winProc.ProcessName, winProc.Threads.Count);
+            //}
+            foreach (ProcessThread winProc in currentThreads)
+            {
+                Console.WriteLine("id {0} state: {1}. start time: {2}, container: {3}", winProc.Id, winProc.ThreadState, winProc.StartTime, winProc.Container);
+            }
+            if (initialThreadCount.Count == currentThreads.Count)
                 //write report to file
                 report.Flush();
         }
 
-        //[After]
-        //public static void dataFlush()
-        //{
 
-        //}
 
     }
 }
